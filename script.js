a211057ef4221c5c8ee39931c4814e8568a90df6
Rosebud <rkle408@gmail.com>
2022-08-31 15:25:16 -0700

    add arrays of possible characters

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Need lists of character types
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// console.log(lowerCase.length) to make sure I have all the letters
+
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+// console.log(upperCase.length) to make sure I have all the letters again
+
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+
+var specialCharacters = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", ']', "^", '_', "`", "{", "|", "}", "~" ]
+// Had to add extra backslash as it affecting everything after; should be 33 characters
+// console.log(specialCharacters.length)
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
