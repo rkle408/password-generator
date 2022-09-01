8dfe6e16ceba2e3380fa4ff01366209d433754c3
Rosebud <rkle408@gmail.com>
2022-08-31 17:33:44 -0700

    first prompt display working

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 // Need lists of character types
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-// console.log(lowerCase.length) to make sure I have all the letters
+// To make sure I have all the letters = 26:
+// console.log(lowerCase.length)
 
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-// console.log(upperCase.length) to make sure I have all the letters again
+// To make sure I have all the letters again = 26:
+// console.log(upperCase.length)
 
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
 
 var specialCharacters = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", ']', "^", '_', "`", "{", "|", "}", "~" ]
-// Had to add extra backslash as it affecting everything after; should be 33 characters
+// Had to add extra backslash as it affecting everything after; should be 33 characters:
 // console.log(specialCharacters.length)
+
+// Recognize that "generatePassword();" is a function that is undefined, so need to define it:
+function generatePassword() {
+  //Test that button is working: 
+  //console.log("Yay! You did it!")
+  window.prompt("How long do you want your password?")
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
